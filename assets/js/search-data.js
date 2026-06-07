122455fb0d3331956c7ebceb446ccb99a3e2eab9
haewon-sim <162613635+haewon-sim@users.noreply.github.com>
2026-06-07 09:55:18 +0000

    Deploying to gh-pages from @ haewon-sim/haewon-sim.github.io@0b2b7ac19f4649539f98eaf8bd100c6d6e3fcbd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/personal/";
           },
+        },{id: "nav-gallery",
+          title: "gallery",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/gallery/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
